5459b607def7d74160c06defdf9731aa2616c59c
mattmory <matt.mory@gmail.com>
2018-07-07 19:33:10 -0700

    Add back game as an object js.

--- assets/javascript/app(AS OBJECT).js
+++ assets/javascript/app(AS OBJECT).js
@@ -1,9 +1,4 @@
-/* declaration of globla variables */
-//var currentScore = 0;
-//var totalCorrect = 0;
-//var totalWrong = 0;
-//var remainingHints = 5;
-//var currentQuestion;
+var currentSeconds;
 
 var triviaGame = {
 
@@ -15,19 +10,22 @@ var triviaGame = {
         this.remainingQuestions = 0;
         this.theQuestions = [];
         this.currentQuestion;
+        this.secondCounter;
+        this.updateCounter;
         this.loadQuestions();
-        that = this;
+        
 
     },
 
     // Function to load the next question
     nextQuestion: function () {
+        clearInterval(this.secondCounter);
         // Step 1: Pick the Question
         var nextQuestionIndex = Math.floor(Math.random() * this.remainingQuestions);
         this.currentQuestion = this.theQuestions[nextQuestionIndex];
 
         // Step 2: Fade the question out, set the question and answers
-        var questionText = that.currentQuestion.quest;
+        var questionText = this.currentQuestion.quest;
         var answerTexts = [];
         // Build an object array of all the answers, then sort by a decimal
         answerTexts[0] = { answer: this.currentQuestion.answer, srtValue: (Math.random() * 1000) };
@@ -35,7 +33,6 @@ var triviaGame = {
         answerTexts[2] = { answer: this.currentQuestion.wrongs[1], srtValue: (Math.random() * 1000) };
         answerTexts[3] = { answer: this.currentQuestion.wrongs[2], srtValue: (Math.random() * 1000) };
         answerTexts.sort((a, b) => parseFloat(a.srtValue) - parseFloat(b.srtValue));
-
 
         $("#gameQuestion").show();
 
@@ -45,31 +42,45 @@ var triviaGame = {
         $("#answer-two").text(answerTexts[1].answer);
         $("#answer-three").text(answerTexts[2].answer);
         $("#answer-four").text(answerTexts[3].answer);
+        $("#disposition-span").text("");
 
-        $(".fadeClass").fadeTo(2000, 1.0, function () {
-            $("#gameDisposition").hide();
-        })
+        $(".fadeClass").fadeTo(1500, 1.0, function () {
 
+        });
+        setTimeout(function () { }, 2000);
+    
 
         // remove the question from the array and decrease the remaining question count
         this.theQuestions.splice(nextQuestionIndex, 1);
         this.remainingQuestions--;
 
-        //  $(".fadeClass").fadeTo(2000, 1.0, function() {console.log("Fade Up")});
+        this.readyForAnswer = true;
+
+        //Start the timer.
+        remainingSeconds = 30;
+        this.secondCounter = setInterval(this.updateCounter, 1000);
+
     },
 
-    // This is the meat of it all.
-    startTimer: function () {
-        console.log("Timer Start.")
+    updateCounter: function () {
+        if (remainingSeconds <= 0) {
+            clearInterval(this.secondCounter);
+            this.questionDone(false);
+        }
+        /* update the progress bar */
+        $("#progbar").attr("style", "width: " + Math.floor((100 * remainingSeconds) / 30) + "%");
+        $("#progbar").attr("aria-valuenow", Math.floor((100 * remainingSeconds) / 30));
+        remainingSeconds--;
     },
 
     // Takes in a boolean, true if the answer is right, false if wrong. Controls the screens and sets up the next question.
     questionDone: function (isCorrect) {
+        clearInterval(this.secondCounter);
         $("#gameQuestion").hide();
         $("#gameDisposition").show();
 
         if (isCorrect) {
-            this.currentScore++;
+            this.currentScore+= 5*remainingSeconds;
             this.totalCorrect++;
             $("#disposition-span").text("You are correct!");
         }
@@ -80,18 +91,19 @@ var triviaGame = {
 
         // After two seconds, fade the disposition container to 0.
 
-
-        var that = this;
         if (this.remainingQuestions >= 1) {
-            $(".fadeClass").fadeTo(2000, 0.0, function () {
+            $(".fadeClass").fadeTo(1500, 0.0, function () { });
+            var that = this;
+            setTimeout(function () {
                 $("#gameDisposition").hide();
                 that.nextQuestion();
-            })}
+            }, 2000);
+        }
+
         else {
             $("#gameDispostion").hide();
             $("#gameEnd").show();
-            $("#gameover-span").text("The game is over. Score: " + this.totalCorrect);
-            return;
+            $("#gameover-span").text("The game is over. You got " + this.totalCorrect + " correct for a total score of " + this.currentScore +".");
         }
     },
 
